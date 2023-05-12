1e28fbaa84edb28bae2e54bc2189c503b49b86fe
safwen-code <safwendjebbi1234@gmail.com>
2023-05-12 15:36:23 +0100

    new idea

--- src/components/content/Project.jsx
+++ src/components/content/Project.jsx
@@ -7,15 +7,15 @@ const Project = () => {
         <h2>Projects</h2>
         <hr class="  border border-info border-2 opacity-100 w-50 ms-4"></hr>
         <p className="lead">
-          I'm Creative Director and UI/UX Designer from Sydney, Australia,
-          working in web development and print media. I enjoy turning complex
-          problems into simple, beautiful and intuitive designs. My aim is to
-          bring across your message and identity in the most creative way. I
-          created web design for many famous brand companies.
+          Software Engineer with 3 years Computer Science degree and more than 2
+          year of experience.As a dedicated problem solver , I have many skill
+          in Javascript ,node js , php and supporting
+          laguages/libraries/frameworks that solve real-word problems through
+          code.
         </p>
       </div>
       <div className="row m-2 border border-dark">
-        <h2>what i do!</h2>
+        <h2>Project I Done</h2>
         <hr class="  border border-info border-2 opacity-100 w-50 ms-4"></hr>
       </div>
 
@@ -23,8 +23,8 @@ const Project = () => {
         <div class="row border border-dark">
           <div class="col-sm-6 col-md-6  border border-dark">
             <div
-              class="card mb-3 text-bg-secondary"
-              style={{ max_width: '540px' }}
+              class="card mb-3 "
+              style={{ max_width: '540px', backgroundColor: '#ECF2FF' }}
             >
               <div class="row g-0">
                 <div class="col-md-4">
@@ -32,15 +32,13 @@ const Project = () => {
                 </div>
                 <div class="col-md-8">
                   <div class="card-body">
-                    <h5 class="card-title">web developpement</h5>
+                    <h5 class="card-title">Jungel Home</h5>
                     <p class="card-text">
-                      This is a wider card with supporting text below as a
-                      natural lead-in to additional content. This content is a
-                      little bit longer.
+                      littel project that display planet of Jungel Home
                     </p>
                     <p class="card-text">
                       <small class="text-body-secondary">
-                        Last updated 3 mins ago
+                        Created 3 years ago
                       </small>
                     </p>
                   </div>
@@ -50,8 +48,8 @@ const Project = () => {
           </div>
           <div class="col-sm-6 col-md-6  border border-dark">
             <div
-              class="card mb-3 text-bg-secondary"
-              style={{ max_width: '540px' }}
+              class="card mb-3 "
+              style={{ max_width: '540px', backgroundColor: '#ECF2FF' }}
             >
               <div class="row g-0">
                 <div class="col-md-4">
@@ -59,15 +57,13 @@ const Project = () => {
                 </div>
                 <div class="col-md-8">
                   <div class="card-body">
-                    <h5 class="card-title">web developpement</h5>
+                    <h5 class="card-title">Timilec Managment</h5>
                     <p class="card-text">
-                      This is a wider card with supporting text below as a
-                      natural lead-in to additional content. This content is a
-                      little bit longer.
+                      Project for manage team position of termodure group
                     </p>
                     <p class="card-text">
                       <small class="text-body-secondary">
-                        Last updated 3 mins ago
+                        Created 2years ago
                       </small>
                     </p>
                   </div>
@@ -77,8 +73,8 @@ const Project = () => {
           </div>
           <div class="col-sm-6 col-md-6  border border-dark">
             <div
-              class="card mb-3 text-bg-secondary"
-              style={{ max_width: '540px' }}
+              class="card mb-3 "
+              style={{ max_width: '540px', backgroundColor: '#ECF2FF' }}
             >
               <div class="row g-0">
                 <div class="col-md-4">
@@ -86,15 +82,11 @@ const Project = () => {
                 </div>
                 <div class="col-md-8">
                   <div class="card-body">
-                    <h5 class="card-title">web developpement</h5>
-                    <p class="card-text">
-                      This is a wider card with supporting text below as a
-                      natural lead-in to additional content. This content is a
-                      little bit longer.
-                    </p>
+                    <h5 class="card-title">Portfolio </h5>
+                    <p class="card-text">Create a portfolio with react</p>
                     <p class="card-text">
                       <small class="text-body-secondary">
-                        Last updated 3 mins ago
+                        Created yesterday
                       </small>
                     </p>
                   </div>
