24f5ce91f8e61da4cce44779af1772b11a9e8fbe
safwen-code <safwendjebbi1234@gmail.com>
2023-05-14 21:44:30 +0100

    education & experience profile

--- src/components/content/Skillmain.jsx
+++ src/components/content/Skillmain.jsx
@@ -6,93 +6,118 @@ const Skillmain = () => {
     <div className="container  m-2 border border-dark  " id="skills">
       <div className="row m-1 border border-dark  ">
         <div className="col-sm-6 col-md-6  border border-primary">
-          <h2>education </h2>
-          <div className="row border border-dark d-flex flex-column align-items-center">
-            <div className=" col-sm-6 col-md-6 border border-dark">
+          <h2
+            className="mb-2  "
+            style={{ color: '#191825', fontFamily: 'cursive' }}
+          >
+            Education{' '}
+          </h2>
+          <div className="row border border-dark d-flex flex-column align-items-center pt-1 pb-1">
+            <div
+              className=" col-sm-6 col-md-6 border border-dark mb-2"
+              style={{ width: '380px' }}
+            >
               <div class="card">
                 <div class="card-body">
-                  <h5 class="card-title">Special title treatment</h5>
                   <p class="card-text">
-                    With supporting text below as a natural lead-in to
-                    additional content.
+                    <small class="text-body-secondary">2020</small>
                   </p>
-                  <a href="#" class="btn btn-primary">
-                    Go somewhere
-                  </a>
+                  <h5 class="card-title">Developpeur MERN</h5>
+                  <p class="card-text">Go My Code</p>
                 </div>
               </div>
             </div>
-            <div className=" col-sm-6 col-md-6 border border-dark">
+            <div
+              className=" col-sm-6 col-md-6 border border-dark mb-2"
+              style={{ width: '380px' }}
+            >
               <div class="card">
                 <div class="card-body">
-                  <h5 class="card-title">Special title treatment</h5>
                   <p class="card-text">
-                    With supporting text below as a natural lead-in to
-                    additional content.
+                    <small class="text-body-secondary">2018-2020</small>
                   </p>
-                  <a href="#" class="btn btn-primary">
-                    Go somewhere
-                  </a>
+                  <h5 class="card-title">Open Classroom</h5>
+                  <p class="card-text">Certificat en ligne</p>
                 </div>
               </div>
             </div>
-            <div className=" col-sm-6 col-md-6 border border-dark">
+            <div
+              className=" col-sm-6 col-md-6 border border-dark mb-2"
+              style={{ width: '380px' }}
+            >
               <div class="card">
                 <div class="card-body">
-                  <h5 class="card-title">Special title treatment</h5>
                   <p class="card-text">
-                    With supporting text below as a natural lead-in to
-                    additional content.
+                    <small class="text-body-secondary">2017</small>
                   </p>
-                  <a href="#" class="btn btn-primary">
-                    Go somewhere
-                  </a>
+                  <h5 class="card-title">Iset</h5>
+                  <p class="card-text">cetificat en ligne</p>
                 </div>
               </div>
             </div>
           </div>
         </div>
         <div className="col-sm-6 col-md-6  border border-primary">
-          <h2>experience</h2>
-          <div className="row border border-dark d-flex flex-column align-items-center">
-            <div className=" col-sm-6 col-md-6 border border-dark">
-              <div class="card">
+          <h2
+            className="mb-2 "
+            style={{ color: '#191825', fontFamily: 'cursive' }}
+          >
+            Experience
+          </h2>
+          <div className="row border border-dark d-flex flex-column align-items-center pt-1 pb-1">
+            <div
+              className=" col-sm-6 col-md-6 border border-dark mb-2"
+              style={{ width: '380px' }}
+            >
+              <div class="card ">
                 <div class="card-body">
-                  <h5 class="card-title">Special title treatment</h5>
                   <p class="card-text">
-                    With supporting text below as a natural lead-in to
-                    additional content.
+                    <small class="text-body-secondary">2021</small>
+                  </p>
+                  <h5 class="card-title">Go My Code</h5>
+                  <p class="card-text">
+                    Intégration de maquette Gestions du state via Redux
+                    Conception du cahier de charge Suivie la méthodologie SCRUM
                   </p>
                   <a href="#" class="btn btn-primary">
-                    Go somewhere
+                    more
                   </a>
                 </div>
               </div>
             </div>
-            <div className=" col-sm-6 col-md-6 border border-dark">
+            <div
+              className=" col-sm-6 col-md-6 border border-dark mb-2"
+              style={{ width: '380px' }}
+            >
               <div class="card">
                 <div class="card-body">
-                  <h5 class="card-title">Special title treatment</h5>
                   <p class="card-text">
-                    With supporting text below as a natural lead-in to
-                    additional content.
+                    <small class="text-body-secondary">2021</small>
+                  </p>
+                  <h5 class="card-title">Timelec</h5>
+                  <p class="card-text">
+                    Controller le produit. Montage du produit à l’aide du
+                    système FT bios. Suivie la méthodologie ...........
                   </p>
                   <a href="#" class="btn btn-primary">
-                    Go somewhere
+                    more
                   </a>
                 </div>
               </div>
             </div>
-            <div className=" col-sm-6 col-md-6 border border-dark">
+            <div
+              className=" col-sm-6 col-md-6 border border-dark mb-2"
+              style={{ width: '380px' }}
+            >
               <div class="card">
                 <div class="card-body">
-                  <h5 class="card-title">Special title treatment</h5>
                   <p class="card-text">
-                    With supporting text below as a natural lead-in to
-                    additional content.
+                    <small class="text-body-secondary">2023</small>
                   </p>
+                  <h5 class="card-title">developeur Full stack</h5>
+                  <p class="card-text">devlopper des crm pour l'industrie'</p>
                   <a href="#" class="btn btn-primary">
-                    Go somewhere
+                    more
                   </a>
                 </div>
               </div>
